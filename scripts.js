ae82f44dbdb7baaf9bd4ce793ac55cbd0fb53514
mike <8876812+imustbreakyou@users.noreply.github.com>
2024-01-16 16:31:17 -0800

    complete project
    implement js form validation
    
    implement final formationg

---
+++ scripts.js
@@ -0,0 +1,47 @@
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirm_password");
+let fail = document.getElementById("fail");
+let passwordMatch = false;
+const input = document.querySelectorAll ('input');
+
+
+password.addEventListener("input", function(){
+    checkPasswordMatch();
+    
+});
+
+confirmPassword.addEventListener("blur", function(){
+    checkPasswordMatch();
+    
+    
+});
+
+function checkPasswordMatch () {
+    if (password.value === confirmPassword.value) {
+        passwordMatch = true;
+        
+        console.log("passwords match!")
+    } else { 
+        console.log("passwords don't match!")
+        passwordMatch = false;
+        fail.style.color = "red";
+        fail.textContent = "passwords don't match!"
+        password.style = "border: red; background: pink";
+        confirmPassword.style = "border: red; background: pink";
+
+    }
+}
+
+
+function validateInput () {
+    if (!this.checkValidity()) {
+        this.style.border = "1px solid red";
+        console.log('Invalid input:', this.name);
+    } else {
+        console.log('Valid input:', this.name);
+    }
+}
+
+input.forEach(input =>  {
+    input.addEventListener ('blur', validateInput)
+})
